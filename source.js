c6540802069fc48fa6a86d913b86b1969a53ad2b
Joshua Bray <josh.c.bray@gmail.com>
2022-05-22 17:48:10 +0700

    Add style with CSS

--- source.js
+++ source.js
@@ -25,17 +25,28 @@ const temp = [];
 const toOperateON = [];
 
 function digitSelected(e) {
-
   if(display.textContent == displayDefault) display.textContent = "";
 
-  let currentDigitSelected = this.getAttribute("class");
+  let currentDigitSelected = this.textContent;
+
+  // Check if the input is floating already
+  if(currentDigitSelected == "." && temp.includes(".")) return;
+
   display.textContent += currentDigitSelected;
   // Store digits in temp
   temp.push(currentDigitSelected);
 };
 
+
+
+
+
+
+const operatorBtns = document.querySelectorAll(".operators > button");
+operatorBtns.forEach((btn) => btn.addEventListener("click", operatorSelected));
+
 function operatorSelected(e) {
-  let currentOperatorSelected = this.getAttribute("class");
+  let currentOperatorSelected = this.textContent;
 
   // Concat displayed numbers + clear temp array (this f() is async - EventListener)
   let number = Number(temp.join(""));
@@ -44,24 +55,17 @@ function operatorSelected(e) {
   toOperateON.push(number);
   toOperateON.push(currentOperatorSelected);
 
-  // // Checking functions
-  // console.log("number: " + number);
-  // console.log("toOperateOn: " + toOperateON);
-
   // Add operator to display window
   display.textContent += currentOperatorSelected;
  };
 
 
-const operatorBtns = document.querySelectorAll(".operators > button");
-operatorBtns.forEach((btn) => btn.addEventListener("click", operatorSelected));
 
-const equalityBtn = document.querySelectorAll(".operators .equals");
-equalityBtn.forEach((btn) => btn.addEventListener("click", equalitySelected));
 
-function equalitySelected() {
-  display.textContent = runOperations();
-};
+
+
+const equalityBtn = document.querySelector(".operators .equals");
+equalityBtn.addEventListener("click", runOperations);
 
 function runOperations() {
 let bidmasOps = ["*", "/", "+", "-"];
@@ -83,8 +87,12 @@ bidmasOps.forEach((op) => {
   }
 });
 // Invalid if /0
-return divideZero? errorMsg: parseFloat(toOperateON[0]).toFixed(4);
+display.textContent = divideZero? errorMsg: parseFloat(toOperateON[0]).toFixed(4);
 }
+
+
+
+
 
 
 const clearBtn = document.querySelector("div .clear");
@@ -95,7 +103,3 @@ function clearSelected() {
   temp.length = 0;
   toOperateON.length = 0;
 }
-
-// function checkForDivideZero(op, b) {
-//   if (op == "/" && b == 0) return true;
-// }
